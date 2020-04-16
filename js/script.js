427b36f0c8ac83174a2ccbd3417fbe826b55878b
Ray Chang <raychang2017@gmail.com>
2020-04-16 23:44:24 +0800

    Add cover in works data

--- js/script.js
+++ js/script.js
@@ -7,72 +7,86 @@ class Portfolio {
 			{
 				title: 'Ray Chang Space',
 				subtitle: '100% Respondsive Portfolio Website',
-				cover: ''
+				cover: '',
+				destination: 'https://raychang2017.github.io/raychang-space/'
 			},
 			{
 				title: 'Horizontal Scrolling Theater with Vue.js',
 				subtitle: '',
-				cover: ''
+				cover: 'https://cdn.dribbble.com/users/3800131/screenshots/11028118/media/8ba2db964d4f882bd59e50050cb09c46.png',
+				destination: 'https://raychang2017.github.io/horizontal-scrolling-theater/'
 			},
 			{
 				title: 'Let’s Make A Linktree Clone',
 				subtitle: 'a new, refined vertion',
-				cover: ''
+				cover: 'https://cdn.dribbble.com/users/3800131/screenshots/10724954/media/529252d97de31baf1548bd817ad6bc1a.png',
+				destination: 'https://raychang2017.github.io/linktree-clone/'
 			},
 			{
 				title: 'Vue.js Find Card Game',
 				subtitle: '',
-				cover: ''
+				cover: 'https://cdn.dribbble.com/users/3800131/screenshots/8192733/media/af0a6f22cb41c347f1c78de1e92b2c3e.png',
+				destination: 'https://codepen.io/raychang2017/full/XWWEvKj'
 			},
 			{
 				title: 'Vue.js Coffee Menu Editor',
 				subtitle: '',
-				cover: ''
+				cover: 'https://cdn.dribbble.com/users/3800131/screenshots/7101781/media/cbab08496ca35c2f443aa01bbd0d4d31.png',
+				destination: 'https://codepen.io/raychang2017/full/MWgOMEL'
 			},
 			{
 				title: 'Vanilla JS Block Memory Game',
-				subtitle: '',
-				cover: ''
+				subtitle: 'How many levels can you achieve',
+				cover: 'https://cdn.dribbble.com/users/3800131/screenshots/10000193/media/5a5acc6684a86a5f46a9b4cd34f4df8e.gif',
+				destination: 'https://codepen.io/raychang2017/full/eYNmMZe'
 			},
 			{
 				title: 'Elegant Vanilla JS Calculator',
 				subtitle: 'which shows the result immediately',
-				cover: ''
+				cover: 'https://cdn.dribbble.com/users/3800131/screenshots/6901484/_____2019-08-02___4.01.34.png',
+				destination: 'https://codepen.io/raychang2017/full/jgLppK'
 			},
 			{
 				title: 'JavaScript Click Game',
 				subtitle: 'How many times can you click in a minute?',
-				cover: ''
+				cover: 'https://cdn.dribbble.com/users/3800131/screenshots/6827214/_____2019-07-21___5.25.09.png',
+				destination: 'https://codepen.io/raychang2017/full/EqjXJN'
 			},
 			{
 				title: 'Draggable LocalStorage To-do List',
 				subtitle: '',
-				cover: ''
+				cover: 'https://cdn.dribbble.com/users/3800131/screenshots/6757018/_____2019-07-09___11.03.44.png',
+				destination: 'https://raychang2017.github.io/draggable-localStorage-todoList/'
 			},
 			{
 				title: 'Day/Night Transition Postcard',
 				subtitle: 'with CSS Keyfram Animation',
-				cover: ''
+				cover: 'https://cdn.dribbble.com/users/3800131/screenshots/6754986/_____2019-07-05___2.28.36.gif',
+				destination: 'https://codepen.io/raychang2017/full/EBGaZQ'
 			},
 			{
 				title: 'Responsive Project Management Web Page',
 				subtitle: 'with CSS Grid & Flexbox',
-				cover: ''
+				cover: 'https://cdn.dribbble.com/users/3800131/screenshots/6736854/_____2019-07-05___2.16.23.png',
+				destination: 'https://codepen.io/raychang2017/full/rEoNzx'
 			},
 			{
 				title: 'Apple Magic Keyboard',
 				subtitle: 'with CSS Grid & Flexbox',
-				cover: ''
+				cover: 'https://cdn.dribbble.com/users/3800131/screenshots/6736810/_____2019-07-05___2.06.35.png',
+				destination: 'https://codepen.io/raychang2017/full/ZdVzmZ'
 			},
 			{
 				title: 'Where Is My Cake!?',
 				subtitle: 'CSS Hover, Active Animation',
-				cover: ''
+				cover: 'https://cdn.dribbble.com/users/3800131/screenshots/6736735/_____2019-07-05___5.34.58.gif',
+				destination: 'https://codepen.io/raychang2017/full/VJqZeY'
 			},
 			{
 				title: 'Interactive Cake',
 				subtitle: 'with animate.css',
-				cover: ''
+				cover: 'https://cdn.dribbble.com/users/3800131/screenshots/6736602/_____2019-07-05___1.51.04.gif',
+				destination: 'https://codepen.io/raychang2017/full/ZdmgMr'
 			}
 		];
 		this.events();
@@ -100,16 +114,22 @@ class Portfolio {
 
 			workEl.innerHTML = `
       <article class="work">
-        <img src="${this.works[i].cover}" alt="${this.works[i].title}">
+        <a href="${this.works[i].destination}">
+          <img src="${this.works[i].cover}" alt="${this.works[i].title}">
+        </a>
         <section>
           <div class="title">${this.works[i].title}</div>
           <div class="subtitle">${this.works[i].subtitle}</div>
-          <div class="number">${this.works.length - i}</div>
+          <div class="number">${this.addZeroToNumberUnderTen(this.works.length - i)}</div>
         </section>
       </article>`;
 
 			this.worksEl.appendChild(workEl);
 		}
+	}
+
+	addZeroToNumberUnderTen(num) {
+		return num < 10 ? '0' + num : num;
 	}
 }
 

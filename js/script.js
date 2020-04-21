9645ada672d076ebdb250da06ba19daf09cd2f42
Ray Chang <raychang2017@gmail.com>
2020-04-22 00:18:24 +0800

    Change work cover path

--- js/script.js
+++ js/script.js
@@ -29,13 +29,13 @@ class Portfolio {
 			{
 				title: '當我按下 Enter 之後的事 — HTTP、API 與非同步跨來源請求',
 				subtitle: 'An Medium blog post',
-				cover: 'https://raw.githubusercontent.com/raychang2017/raychang-space/master/img/medium-logo.png',
+				cover: 'https://raw.githubusercontent.com/raychang2017/raychang-space/master/img/medium-logo-transparent.png',
 				destination: 'https://medium.com/@raychangdesign/%E7%95%B6%E6%88%91%E6%8C%89%E4%B8%8B-enter-%E4%B9%8B%E5%BE%8C%E7%9A%84%E4%BA%8B-http-api-%E8%88%87%E9%9D%9E%E5%90%8C%E6%AD%A5%E8%B7%A8%E4%BE%86%E6%BA%90%E8%AB%8B%E6%B1%82-13bcaa426bc0'
 			},
 			{
 				title: 'Node.js 前後端分離與 NPM 套件管理',
 				subtitle: 'An Medium blog post',
-				cover: 'https://raw.githubusercontent.com/raychang2017/raychang-space/master/img/medium-logo.png',
+				cover: 'https://raw.githubusercontent.com/raychang2017/raychang-space/master/img/medium-logo-transparent.png',
 				destination: 'https://medium.com/@raychangdesign/node-js-%E5%89%8D%E5%BE%8C%E7%AB%AF%E5%88%86%E9%9B%A2%E8%88%87-npm-%E5%A5%97%E4%BB%B6%E7%AE%A1%E7%90%86-4f39ba1c315a'
 			},
 			{
@@ -47,7 +47,7 @@ class Portfolio {
 			{
 				title: 'Git 與 Github 是什麼？如何使用 Git？',
 				subtitle: 'An Medium blog post',
-				cover: 'https://raw.githubusercontent.com/raychang2017/raychang-space/master/img/medium-logo.png',
+				cover: 'https://raw.githubusercontent.com/raychang2017/raychang-space/master/img/medium-logo-transparent.png',
 				destination: 'https://medium.com/@raychangdesign/git-%E8%88%87-github-%E6%98%AF%E4%BB%80%E9%BA%BC-%E5%A6%82%E4%BD%95%E4%BD%BF%E7%94%A8-git-fc61f3cd0948'
 			},
 			{
@@ -59,7 +59,7 @@ class Portfolio {
 			{
 				title: '快速精通 JavaScript 的 this',
 				subtitle: 'An Medium blog post',
-				cover: 'https://raw.githubusercontent.com/raychang2017/raychang-space/master/img/medium-logo.png',
+				cover: 'https://raw.githubusercontent.com/raychang2017/raychang-space/master/img/medium-logo-transparent.png',
 				destination: 'https://medium.com/@raychangdesign/%E5%BF%AB%E9%80%9F%E7%B2%BE%E9%80%9A-javascript-%E7%9A%84-this-3f707e0bf873'
 			},
 			{
@@ -71,7 +71,7 @@ class Portfolio {
 			{
 				title: '搞懂網頁技術名詞：MVC、SPA、SSR、AMP 及 PWA',
 				subtitle: 'An Medium blog post',
-				cover: 'https://raw.githubusercontent.com/raychang2017/raychang-space/master/img/medium-logo.png',
+				cover: 'https://raw.githubusercontent.com/raychang2017/raychang-space/master/img/medium-logo-transparent.png',
 				destination: 'https://medium.com/@raychangdesign/%E6%90%9E%E6%87%82%E7%B6%B2%E9%A0%81%E6%8A%80%E8%A1%93%E5%90%8D%E8%A9%9E-mvc-spa-ssr-amp-%E5%8F%8A-pwa-17adfd3d7f7c'
 			},
 			{
@@ -89,7 +89,7 @@ class Portfolio {
 			{
 				title: 'JavaScript 簡介：ES6 以及更早之前的版本',
 				subtitle: 'An Medium blog post',
-				cover: 'https://raw.githubusercontent.com/raychang2017/raychang-space/master/img/medium-logo.png',
+				cover: 'https://raw.githubusercontent.com/raychang2017/raychang-space/master/img/medium-logo-transparent.png',
 				destination: 'https://medium.com/@raychangdesign/javascript-%E7%B0%A1%E4%BB%8B-es6-%E4%BB%A5%E5%8F%8A%E6%9B%B4%E6%97%A9%E4%B9%8B%E5%89%8D%E7%9A%84%E7%89%88%E6%9C%AC-7c78e80248f9'
 			},
 			{

772979a7cabf896b94eace9ccf2b099eec4c3a1d
Ray Chang <39514595+rayc2045@users.noreply.github.com>
2020-09-20 17:43:23 +0800

    Change works data

--- js/script.js
+++ js/script.js
@@ -46,23 +46,11 @@ class Portfolio {
 			},
 			{
 				title: 'Linktree Clone',
-				subtitle: 'Resizable social media link-sharing page',
+				subtitle: 'Resizable link-sharing page',
 				cover:'https://cdn.dribbble.com/users/3800131/screenshots/10724954/media/529252d97de31baf1548bd817ad6bc1a.png',
 				github:'https://github.com/rayc2045/f2e-portfolio/tree/master/12%20-%20%E5%81%9A%E4%B8%80%E5%80%8B%E7%B2%BE%E7%BE%8E%E7%89%88%E6%9C%AC%E7%9A%84%20Linktree%20Clone%20%E2%80%94%20%E8%AA%B0%E8%AA%AA%20Instagram%20bio%20%E5%8F%AA%E8%83%BD%E6%94%BE%E4%B8%80%E5%80%8B%E7%B6%B2%E5%9D%80',
 				destination: 'https://rayc2045.github.io/linktree-clone/'
 			},
-			// {
-			// 	title: '當我按下 Enter 之後的事 — HTTP、API 與非同步跨來源請求',
-			// 	subtitle: 'An Medium blog post',
-			// 	cover: 'https://raw.githubusercontent.com/rayc2045/raychang-space/master/img/medium-logo-transparent.png',
-			// 	destination: 'https://medium.com/@raychangdesign/%E7%95%B6%E6%88%91%E6%8C%89%E4%B8%8B-enter-%E4%B9%8B%E5%BE%8C%E7%9A%84%E4%BA%8B-http-api-%E8%88%87%E9%9D%9E%E5%90%8C%E6%AD%A5%E8%B7%A8%E4%BE%86%E6%BA%90%E8%AB%8B%E6%B1%82-13bcaa426bc0'
-			// },
-			// {
-			// 	title: 'Node.js 前後端分離與 NPM 套件管理',
-			// 	subtitle: 'An Medium blog post',
-			// 	cover: 'https://raw.githubusercontent.com/rayc2045/raychang-space/master/img/medium-logo-transparent.png',
-			// 	destination: 'https://medium.com/@raychangdesign/node-js-%E5%89%8D%E5%BE%8C%E7%AB%AF%E5%88%86%E9%9B%A2%E8%88%87-npm-%E5%A5%97%E4%BB%B6%E7%AE%A1%E7%90%86-4f39ba1c315a'
-			// },
 			{
 				title: 'Coffee Menu Editor',
 				subtitle: 'Editing ingredients is no longer difficult',
@@ -70,12 +58,6 @@ class Portfolio {
 				github:'https://github.com/rayc2045/f2e-portfolio/tree/master/10%20-%20Menu%20%E5%AE%A2%E8%A3%BD%E5%8C%96%E4%B8%8D%E9%BA%BB%E7%85%A9%EF%BC%8C%E7%94%A8%20Vue%20%E5%81%9A%E4%B8%80%E5%80%8B%E5%92%96%E5%95%A1%E8%8F%9C%E5%96%AE%E7%B7%A8%E8%BC%AF%E5%99%A8',
 				destination: 'https://codepen.io/rayc2045/full/MWgOMEL'
 			},
-			// {
-			// 	title: 'Git 與 Github 是什麼？如何使用 Git？',
-			// 	subtitle: 'An Medium blog post',
-			// 	cover: 'https://raw.githubusercontent.com/rayc2045/raychang-space/master/img/medium-logo-transparent.png',
-			// 	destination: 'https://medium.com/@raychangdesign/git-%E8%88%87-github-%E6%98%AF%E4%BB%80%E9%BA%BC-%E5%A6%82%E4%BD%95%E4%BD%BF%E7%94%A8-git-fc61f3cd0948'
-			// },
 			{
 				title: 'JS Block Memory Game',
 				subtitle: 'How many levels can you accomplish?',
@@ -83,12 +65,6 @@ class Portfolio {
 				github:'https://github.com/rayc2045/f2e-portfolio/tree/master/09%20-%20%E6%96%B9%E5%A1%8A%E8%A8%98%E6%86%B6%E9%81%8A%E6%88%B2%20%E2%80%93%20%E4%BD%A0%E8%83%BD%E7%8E%A9%E5%88%B0%E7%AC%AC%E5%B9%BE%E9%97%9C%EF%BC%9F',
 				destination: 'https://codepen.io/rayc2045/full/eYNmMZe'
 			},
-			// {
-			// 	title: '快速精通 JavaScript 的 this',
-			// 	subtitle: 'An Medium blog post',
-			// 	cover: 'https://raw.githubusercontent.com/rayc2045/raychang-space/master/img/medium-logo-transparent.png',
-			// 	destination: 'https://medium.com/@raychangdesign/%E5%BF%AB%E9%80%9F%E7%B2%BE%E9%80%9A-javascript-%E7%9A%84-this-3f707e0bf873'
-			// },
 			{
 				title: 'Elegant Vanilla JS Calculator',
 				subtitle: 'Why not shows the result immediately?',
@@ -96,12 +72,6 @@ class Portfolio {
 				github:'https://github.com/rayc2045/f2e-portfolio/tree/master/08%20-%20%E5%81%9A%E4%B8%80%E5%80%8B%E5%84%AA%E9%9B%85%E7%9A%84%20Vanilla%20JS%20%E8%A8%88%E7%AE%97%E6%A9%9F%EF%BC%8C%E5%8D%B3%E6%99%82%E9%A1%AF%E7%A4%BA%E8%A8%88%E7%AE%97%E7%B5%90%E6%9E%9C',
 				destination: 'https://rayc2045.github.io/elegant-vanilla-js-calculator/'
 			},
-			// {
-			// 	title: '搞懂網頁技術名詞：MVC、SPA、SSR、AMP 及 PWA',
-			// 	subtitle: 'An Medium blog post',
-			// 	cover: 'https://raw.githubusercontent.com/rayc2045/raychang-space/master/img/medium-logo-transparent.png',
-			// 	destination: 'https://medium.com/@raychangdesign/%E6%90%9E%E6%87%82%E7%B6%B2%E9%A0%81%E6%8A%80%E8%A1%93%E5%90%8D%E8%A9%9E-mvc-spa-ssr-amp-%E5%8F%8A-pwa-17adfd3d7f7c'
-			// },
 			{
 				title: 'JavaScript Click Game',
 				subtitle: 'How many scores can you get?',
@@ -116,12 +86,6 @@ class Portfolio {
 				github:'https://github.com/rayc2045/f2e-portfolio/tree/master/06%20-%20%E5%8F%AF%E6%8B%96%E6%8B%89%E4%BB%BB%E5%8B%99%E7%9A%84%20LocalStorage%20%E5%BE%85%E8%BE%A6%E4%BA%8B%E9%A0%85%E6%B8%85%E5%96%AE',
 				destination: 'https://rayc2045.github.io/draggable-localStorage-todoList/'
 			},
-			// {
-			// 	title: 'JavaScript 簡介：ES6 以及更早之前的版本',
-			// 	subtitle: 'An Medium blog post',
-			// 	cover: 'https://raw.githubusercontent.com/rayc2045/raychang-space/master/img/medium-logo-transparent.png',
-			// 	destination: 'https://medium.com/@raychangdesign/javascript-%E7%B0%A1%E4%BB%8B-es6-%E4%BB%A5%E5%8F%8A%E6%9B%B4%E6%97%A9%E4%B9%8B%E5%89%8D%E7%9A%84%E7%89%88%E6%9C%AC-7c78e80248f9'
-			// },
 			{
 				title: 'Day/Night Transition Postcard',
 				subtitle: 'Absolutely the modern way to share story',
@@ -145,7 +109,7 @@ class Portfolio {
 			},
 			{
 				title: 'Medium Blog',
-				subtitle: 'Read my technical articles',
+				subtitle: 'Read my latest technical article',
 				cover: 'https://raw.githubusercontent.com/rayc2045/raychang-space/master/img/medium-logo-transparent.png',
 				destination: 'https://medium.com/@raychangdesign/'
 			}
@@ -177,16 +141,8 @@ class Portfolio {
 			if (!this.isTouchDevice) this.antiMouseMove(e, this.nameEl, 80);
 		};
 		document.onscroll = () => {
-			// this.scrollSmoothly(document.documentElement, 1200 ,1200);
 			this.toggleElOverflow(this.mainEl);
 		};
-		// document.addEventListener('wheel',function (event){
-		// 	//only vertical scroll
-		// 	if (event.deltaY > 0) {
-		// 		event.preventDefault();
-		// 		// smoothScroll(document.documentElement, 100, 1000)
-		// 	}
-		// })
 
 		this.parallax();
 		this.updateDate();
@@ -204,7 +160,7 @@ class Portfolio {
 
 		// All HTML elements load finished
 		window.onload = () => {
-			this.hideLoadingEl();
+			this.hideLoadingPage();
 			this.scrollEnable();
 			this.backgroundMusicEl.play();
 		};
@@ -231,20 +187,6 @@ class Portfolio {
 		document.documentElement.style.overflow = 'visible';
 		// document.documentElement.style.marginRight = "0px";
 		// document.documentElement.style.paddingRight = "0px";
-	}
-
-	// TODO: 平滑滾動 https://stackoverflow.com/questions/47011055/smooth-vertical-scrolling-on-mouse-wheel-in-vanilla-javascript
-	scrollSmoothly(el, pixel, delay) {
-		const intervalToRepeat = 25;
-		const step = (intervalToRepeat * pixel) / delay;
-
-		if ( step < pixel) {
-			el.scrollTop += step;
-
-			setTimeout(() => {
-				this.scrollSmoothly(el, pixel - step, delay)
-			}, intervalToRepeat);
-		}
 	}
 
 	parallax() {
@@ -287,11 +229,12 @@ class Portfolio {
 	// 	}
 	// }
 
-	hideLoadingEl() {
+	hideLoadingPage() {
 		this.loadingEl.classList.add('animated');
 
 		setTimeout(() => {
 			this.loadingEl.classList.add('hide');
+			this.loadingEl.remove();
 		}, 2000)
 	}
 	
@@ -338,7 +281,7 @@ class Portfolio {
         <section>
           <div class="number">${this.addZeroToNumberUnderTen(this.works.length - i)}</div>
           <div class="title">${this.works[i].title}</div>
-          <div class="subtitle">${this.works[i].subtitle}${this.works[i].github ? ` <a class="link-github" href="${this.works[i].github}" target="_blank">(info)</a>` : ''}</div>
+          <div class="subtitle">${this.works[i].subtitle}${this.works[i].github ? ` (<a class="link-github" href="${this.works[i].github}" target="_blank">info</a>)` : ''}</div>
         </section>`;
 
 			this.worksEl.appendChild(listEl);

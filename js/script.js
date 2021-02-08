967ffa09f6d415c58bcdd0d7d67c7657d5663bfc
Ray C <rayc2045@gmail.com>
2021-02-08 13:16:52 +0800

    Optimize scroll and hover interactions

--- js/script.js
+++ js/script.js
@@ -98,6 +98,13 @@ class Portfolio {
     if (!this.isTouchDevice) {
       // document.onmousemove = e => this.antiMouseMove(e, this.nameEl, 80);
       this.activateHoverInteraction(this.worksEl, this.footerEl);
+      // Combine scroll and hover interactions
+      this.worksEl.childNodes.forEach(el => {
+        el.childNodes[1].onmouseenter = () =>
+          this.worksEl.childNodes.forEach(el => this.toggleGrayscale(el))
+        el.childNodes[1].onmouseout = () =>
+          this.worksEl.childNodes.forEach(el => el.classList.remove('grayscale'))
+      });
       this.parallax();
       if (!this.isFirefox) this.smoothScroll();
     }
@@ -123,13 +130,6 @@ class Portfolio {
       this.soundPlay(this.pagingSound);
       window.scrollTo(0, 0);
     };
-
-    this.worksEl.childNodes.forEach(el => {
-      el.childNodes[1].onmouseenter = () =>
-        this.worksEl.childNodes.forEach(el => this.toggleGrayscale(el))
-      el.childNodes[1].onmouseout = () =>
-        this.worksEl.childNodes.forEach(el => el.classList.remove('grayscale'))
-    });
 
     this.formEl.onkeydown = e => {
       // console.log(e.target.value);
